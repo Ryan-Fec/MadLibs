10b732ebdb5433963297b1eaeff1e8aef1c452d7
Ryan Fecarotta <ryan.fecarotta@students.highline.edu>
2022-01-18 18:15:42 -0800

    Safe version

--- MadLibs/FunctionalCode.cpp
+++ MadLibs/FunctionalCode.cpp
@@ -1,11 +1,29 @@
 #include <iostream>
+#include <string>
+void MadLib(std::string word) {
+	if (word[0] == "A" or "O" or "I" or "U" or "E")
+		std::cout << "Give me an" << word << ": ";
+	else
+		std::cout std:: << "Give me a" << word << ": ";
+	std::string word;
+	std::cin word;
+}
 
 int main() {
 	std::cout << "Give me an occupation: ";
 	std::string occu;
 	std::cin >> occu;
-	std::cout << "\nGive me an object: ";
+	std::cout << "Give me an object: ";
 	std::string obj;
 	std::cin >> obj;
-	std::cout << "The " << occu << " went to the " << obj << " store.";
+	std::cout << "Give me an adverb: ";
+	std::string adv;
+	std::cin >> adv;
+	std::cout << "Give me a verb: ";
+	std::string verb;
+	std::cin >> verb;
+	std::cout << "Give me a food: ";
+	std::string food;
+	std::cin >> food;
+	std::cout << "The " << occu << " went to the " << obj << " store. Then, they " << adv << verb <<;
 }
